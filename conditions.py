abad099acb5c8455d54e64be785e07ae4b0e7f8e
s1056768 <71049403+s1056768@users.noreply.github.com>
2020-10-12 11:34:17 -0400

    Conditions

---
+++ conditions.py
@@ -0,0 +1,17 @@
+myvar = "Temp"
+#begin loop
+Temp = int (input ("Please enter current temperature:"))
+if Temp > 90 :
+    print ("Wear Shorts")
+elif Temp > 70 :
+    print ("Short sleeves are fine") ;
+elif Temp > 50 :
+    print ("Wear a jacket")
+elif Temp > 32 :
+    print ("Wear a heavy coat")
+    
+else: print ("Stay Inside")   
+
+#end loop
+
+print("stopping now")    
